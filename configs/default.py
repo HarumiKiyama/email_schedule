b678f41571a0d1726e4494f89369a609ed94b538
harumi <h.kiyama0720@gmail.com>
2019-12-17 22:25:15 +0800

    add configs

---
+++ configs/default.py
@@ -0,0 +1,11 @@
+SWAGGER = {'doc_path': '/docs'}
+
+PW_DB_URL = 'mysql+pool://mysql:3306/'
+PW_CONN_PARAMS = {
+    'max_connections': 2,
+    'charset': 'utf8mb4',
+    'stale_timeout': 3600,
+    'database': 'mail_test',
+    'user': 'root',
+    'password': '12345678'
+}
