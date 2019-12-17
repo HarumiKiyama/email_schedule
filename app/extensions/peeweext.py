8c4fe9299138ad2e114d5db2709a23c57ee697c4
harumi <h.kiyama0720@gmail.com>
2019-12-17 22:22:49 +0800

    refactor extensions

---
+++ app/extensions/peeweext.py
@@ -0,0 +1,27 @@
+from peewee import Model
+from playhouse import db_url
+from werkzeug.utils import cached_property
+
+
+class Peeweext:
+    def __init__(self, ns='PW_'):
+        self.ns = ns
+
+    def init_app(self, app):
+        opts = app.config.get_namespace(self.ns)
+        conn_params = opts.get('conn_params', {})
+        self.database = db_url.connect(opts['db_url'], **conn_params)
+
+    @cached_property
+    def Model(self):
+        class BaseModel(Model):
+            class Meta:
+                database = self.database
+
+        return BaseModel
+
+    def connect_db(self):
+        self.database.connect()
+
+    def close_db(self):
+        self.database.close()
