33f2cf6201be3ec64d714f6710b40688a4758128
harumi <h.kiyama0720@gmail.com>
2019-12-19 20:54:24 +0800

    fix default config

--- configs/default.py
+++ configs/default.py
@@ -3,12 +3,12 @@ SWAGGER = {
     'openapi': '3.0.2',
 }
 
-PW_DB_URL = 'mysql+pool://mysql:3306/'
+PW_DB_URL = 'mysql+pool://127.0.0.1:3306/'
 PW_CONN_PARAMS = {
     'max_connections': 2,
     'charset': 'utf8mb4',
     'stale_timeout': 3600,
-    'database': 'mail_test',
+    'database': 'email_test',
     'user': 'root',
     'password': '12345678'
 }
