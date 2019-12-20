7d58678280394b970e186abbfc95461251a855da
harumi <h.kiyama0720@gmail.com>
2019-12-20 20:39:55 +0800

    add new api

--- app/api/urls.py
+++ app/api/urls.py
@@ -1,6 +1,8 @@
 from flask import Blueprint
-from .views import EmailScheduleView
+from .views import EmailScheduleView, EmailTrackView
 
 bp = Blueprint('api', __name__)
 bp.add_url_rule('/email/schedule',
                 view_func=EmailScheduleView.as_view('email_schedule'))
+bp.add_url_rule('/email/track',
+                view_func=EmailTrackView.as_view('email_track'))
--- app/api/views.py
+++ app/api/views.py
@@ -2,6 +2,7 @@ from flask.views import MethodView
 from flask import request, jsonify
 import datetime
 from app.models import EmailLog
+from app.extensions import emailext
 
 
 class EmailScheduleView(MethodView):
@@ -12,10 +13,21 @@ class EmailScheduleView(MethodView):
             send_at = data['eta']
         else:
             countdown = data['countdown']
-            send_at = datetime.datetime.now() + datetime.timedelta(
-                **countdown)
+            send_at = datetime.datetime.now() + datetime.timedelta(**countdown)
         EmailLog.create(subject=data['subject'],
                         body=data['body'],
                         to=data['to'],
                         send_at=send_at)
         return jsonify()
+
+
+class EmailTrackView(MethodView):
+    def post(self):
+        data = request.get_json()
+        if not emailext.verify(**data['signature']):
+            return jsonify()
+        event_data = data['event-data']
+        email_id = event_data['id']
+        email = EmailLog.select().where(EmailLog.email_id == email_id).first()
+        if email is not None:
+            pass
