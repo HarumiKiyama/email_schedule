ba0e572b17da2590ebe72bccb3237edfad7c0163
harumi <h.kiyama0720@gmail.com>
2019-12-26 22:54:16 +0800

    finish webhooks

--- app/api/urls.py
+++ app/api/urls.py
@@ -1,8 +1,11 @@
 from flask import Blueprint
-from .views import EmailScheduleView, EmailTrackView
+from .views import EmailScheduleView, EmailTrackOpenView, EmailTrackSendView
 
 bp = Blueprint('api', __name__)
 bp.add_url_rule('/email/schedule',
                 view_func=EmailScheduleView.as_view('email_schedule'))
-bp.add_url_rule('/email/track',
-                view_func=EmailTrackView.as_view('email_track'))
+bp.add_url_rule('/email/send-track',
+                view_func=EmailTrackSendView.as_view('email_send_track'))
+
+bp.add_url_rule('/email/open-track',
+                view_func=EmailTrackOpenView.as_view('email_open_track'))
--- app/api/views.py
+++ app/api/views.py
@@ -1,33 +1,74 @@
-from flask.views import MethodView
-from flask import request, jsonify
 import datetime
+
+from flask.views import MethodView
+from flask import request, jsonify, Response
+
+from app.exceptions import ScheduleTimeException
 from app.models import EmailLog
-from app.extensions import emailext
+from app.extensions import mailext
+from app.tasks import send_email
 
 
 class EmailScheduleView(MethodView):
     def post(self):
         data = request.get_json()
+        now = datetime.datetime.now()
         if data.get('eta') is not None:
-            # TODO: check whether eta is greater than today
-            send_at = data['eta']
+            send_at = datetime.datetime.strptime(data['eta'],
+                                                 '%Y-%m-%d %H:%M:%S')
+            if send_at < now:
+                raise ScheduleTimeException(
+                    'schedule time:{} error'.format(send_at))
         else:
             countdown = data['countdown']
             send_at = datetime.datetime.now() + datetime.timedelta(**countdown)
-        EmailLog.create(subject=data['subject'],
-                        body=data['body'],
-                        to=data['to'],
-                        send_at=send_at)
+
+        email = EmailLog.create(subject=data['subject'],
+                                body=data['body'],
+                                to=data['to'],
+                                send_at=send_at)
+        if send_at - now <= datetime.timedelta(minutes=15):
+            send_email.apply_async(args=(email.id, ), eta=send_at)
         return jsonify()
 
 
-class EmailTrackView(MethodView):
+class EmailTrackSendView(MethodView):
     def post(self):
         data = request.get_json()
-        if not emailext.verify(**data['signature']):
-            return jsonify()
         event_data = data['event-data']
+        if event_data['event'] not in (
+                'delivered',
+                'failed',
+        ) or not mailext.verify(**data['signature']):
+            return Response(status=406)
         email_id = event_data['id']
         email = EmailLog.select().where(EmailLog.email_id == email_id).first()
-        if email is not None:
-            pass
+        if email is None or email.status != EmailLog.SEND_MAILGUN:
+            return Response(status=406)
+        if event_data['event'] == 'failed':
+            EmailLog.set_by_id(email.id, {'status': EmailLog.SEND_FAILED})
+        else:
+            EmailLog.set_by_id(
+                email.id, {
+                    'status': EmailLog.SEND_SUCCESSFULLY,
+                    'updated_at': datetime.datetime.now()
+                })
+        return Response(status=200)
+
+
+class EmailTrackOpenView(MethodView):
+    def post(self):
+        data = request.get_json()
+        event_data = data['event-data']
+        if event_data['event'] != 'opened' or not mailext.verify(
+                **data['signature']):
+            return Response(status=406)
+        email_id = event_data['id']
+        email = EmailLog.select().where(EmailLog.email_id == email_id).first()
+        if email is None or email.status != EmailLog.SEND_SUCCESSFULLY:
+            return Response(status=406)
+        EmailLog.set_by_id(email.id, {
+            'status': EmailLog.OPENED,
+            'updated_at': datetime.datetime.now()
+        })
+        return Response(status=200)
